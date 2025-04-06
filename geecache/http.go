e7e75fe9672f07becfa36bbf2d1da2e2d3a72e71
Chriszzz <chrisz2zz@duck.com>
2025-04-06 18:55:12 +0800

    feat: day3

---
+++ geecache/http.go
@@ -0,0 +1,58 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+type HTTPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPool {
+	return &HTTPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPool serving unexpected path: " + r.URL.Path)
+	}
+
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
